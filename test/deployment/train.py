5a079e67ff541af16265a3ec89c62633a9999343
Naila Farooqui <gtnaila@gmail.com>
2024-11-28 11:50:10 -0800

    update train.py

--- test/deployment/train.py
+++ test/deployment/train.py
@@ -1,13 +1,13 @@
 import masint
-import asyncio
 
-masint.api_url = "https://greg1232--cray-cpu-llama-3-2-1b-fastapi-app.modal.run"
-# masint.api_url = "https://greg1232--cray-nvidia-llama-3-2-3b-instruct-fastapi-app.modal.run"
+# masint.api_url = "https://greg1232--cray-cpu-llama-3-2-1b-fastapi-app.modal.run"
+masint.api_url = "https://greg1232--cray-nvidia-llama-3-2-3b-instruct-fastapi-app.modal.run"
 
-count = 10
 def get_dataset():
     dataset = []
 
+    count = 1
+    
     for i in range(count):
         dataset.append(
             {"input": f"What is {i} + {i}", "output": "The answer is " + str(i + i)}
@@ -19,7 +19,7 @@ llm = masint.SupermassiveIntelligence()
 
 dataset = get_dataset()
 
-status = asyncio.run(llm.train(dataset, train_args={"max_steps": (100*count)}))
+status = llm.train(dataset, train_args={"max_steps": 11})
 
 print(status)
 
