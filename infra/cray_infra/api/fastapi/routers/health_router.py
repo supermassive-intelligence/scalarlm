746e6b4005b42e50f729dbea76d6468999dad975
Greg Diamos <gregory.diamos@gmail.com>
2025-11-15 00:34:19 -0800

    added cli logs

--- infra/cray_infra/api/fastapi/routers/health_router.py
+++ infra/cray_infra/api/fastapi/routers/health_router.py
@@ -1,8 +1,14 @@
 from cray_infra.api.fastapi.health.check_health import check_health
+from cray_infra.api.fastapi.health.service_logs_generator import service_logs_generator
 
 from fastapi import APIRouter
 
 from fastapi.responses import JSONResponse
+
+from starlette.responses import StreamingResponse
+
+import traceback
+
 import logging
 
 logger = logging.getLogger(__name__)
@@ -18,6 +24,19 @@ async def health():
 @health_router.get("/keepalive")
 async def health():
     return {"status": "ok"}
+
+
+@health_router.get("/logs/{service_name}")
+async def get_service_logs(service_name: str, starting_line_number: int = 0):
+    try:
+        return StreamingResponse(
+            content=service_logs_generator(service_name, starting_line_number),
+            media_type="text/event-stream",
+        )
+    except Exception as e:
+        logger.exception(e)
+        logger.error(traceback.format_exc())
+        return JSONResponse(content={"error": str(e)}, status_code=500)
 
 
 @health_router.get("/endpoints")
