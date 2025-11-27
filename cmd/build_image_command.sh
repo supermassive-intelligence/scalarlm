c98a5f2aea83ca6756567b0a3229799e7a9e77b4
Naila Farooqui <gtnaila@gmail.com>
2025-11-26 17:59:12 -0800

    Qwen235B deployment (#139)
    * Adding tensor-parallel-size and qwen280b deployment.
    
    * update
    
    * fix build-image cmd
    
    * updates
    
    * updates

--- cmd/build_image_command.sh
+++ cmd/build_image_command.sh
@@ -28,6 +28,7 @@ else
 fi
 
 docker_build_command="docker build --platform ${docker_platform} --build-arg BASE_NAME=${target} --build-arg TORCH_CUDA_ARCH_LIST=${sm_arch} --build-arg VLLM_TARGET_DEVICE=${vllm_target_device} -t cray:latest ."
+mkdir -p vllm
 
 # Run docker build command
 echo $(green_bold Building image with command: ${docker_build_command})
