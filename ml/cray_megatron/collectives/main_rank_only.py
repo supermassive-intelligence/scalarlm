b77a8a060a22b3b41c088b53fa9c28866d5fb5e5
Greg Diamos <3401278+greg1232@users.noreply.github.com>
2026-03-28 20:13:53 -0700

    Greg.deadlock (#165)
    * stability improvements for 256 gpu runs.
    
    * debugging lora
    
    * integrate opentelco repo
    
    * exception printing on multiple ranks
    
    * opentelco improvements
    
    * handle deadlock and vllm args
    
    ---------
    
    Co-authored-by: Greg Diamos <gregory.diamos@gmail.com>

--- ml/cray_megatron/collectives/main_rank_only.py
+++ ml/cray_megatron/collectives/main_rank_only.py
@@ -1,17 +1,26 @@
-
+from functools import wraps
 from gpu_aware_mpi import get_rank, barrier
-
 
 def is_main_rank():
     return get_rank() == 0
 
-def main_rank_only(func):
-    def wrap_function(*args, **kwargs):
-        result = None
-        barrier()
-        if is_main_rank():
-            result = func(*args, **kwargs)
-        barrier()
-        return result
+_in_main_rank_only = False
 
-    return wrap_function
+def main_rank_only(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        global _in_main_rank_only
+
+        if _in_main_rank_only:
+            return func(*args, **kwargs)
+
+        _in_main_rank_only = True
+        try:
+            barrier()
+            result = func(*args, **kwargs) if is_main_rank() else None
+            barrier()
+            return result
+        finally:
+            _in_main_rank_only = False
+
+    return wrapper
