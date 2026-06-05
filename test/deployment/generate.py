a0bfcaf7c1dba85fd2a01ea11e0260abe1ac0409
Naila Farooqui <gtnaila@gmail.com>
2026-06-05 16:58:26 -0700

    Cleanup of test scripts (#197)

--- test/deployment/generate.py
+++ test/deployment/generate.py
@@ -1,15 +1,19 @@
 import scalarlm
+import argparse
 
-scalarlm.api_url = "http://localhost:8000"
+parser = argparse.ArgumentParser()
+parser.add_argument("--api-url", default="http://localhost:8000")
+parser.add_argument("--model", default=None)
+args = parser.parse_args()
 
-
+scalarlm.api_url = args.api_url
 llm = scalarlm.SupermassiveIntelligence()
 
 dataset = ["Hello, how are you?", "What is 10 + 15?", "Tell me a story"]
 
 results = llm.generate(
+    model_name=args.model,
     prompts=dataset,
     max_tokens=32,
 )
-
-print(results)
+print(f"model:\n{args.model}\n\nresults:\n{results}")
--- test/deployment/train.py
+++ test/deployment/train.py
@@ -1,31 +1,31 @@
 import scalarlm
+import argparse
 
-scalarlm.api_url = "http://localhost:8000"
+parser = argparse.ArgumentParser()
+parser.add_argument("--api-url", default="http://localhost:8000")
+args = parser.parse_args()
+
+scalarlm.api_url = args.api_url
 
 def get_dataset():
     dataset = []
-
     count = 1
-
     for i in range(count):
         dataset.append({"input": f"What is {i} + {i}?", "output": str(i + i)})
-
     return dataset
 
-
 llm = scalarlm.SupermassiveIntelligence()
-
 dataset = get_dataset()
-
 status = llm.train(
     dataset,
-    train_args={"max_steps": 10, 
-		"learning_rate": 3e-3, 
-		"gpus": 1, 
-		"nodes" : 1,
-            	"max_token_block_size": 4096,
-	    	"adapter_type": "lora",
-            	"steps_per_checkpoint": 10000},
+    train_args={
+        "max_steps": 10,
+        "learning_rate": 3e-3,
+        "gpus": 1,
+        "nodes": 1,
+        "max_token_block_size": 4096,
+        "adapter_type": "lora",
+        "steps_per_checkpoint": 10000
+    },
 )
-
 print(status)
